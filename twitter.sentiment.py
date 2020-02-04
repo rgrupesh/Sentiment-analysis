762d6d9eebfbf4e7c5e08e0d342c3993be02ac41
rgrupesh <54068868+rgrupesh@users.noreply.github.com>
2020-02-04 20:17:03 +0545

    Update twitter.sentiment.py

--- twitter.sentiment.py
+++ twitter.sentiment.py
@@ -16,5 +16,8 @@ public_tweets= api.search('Messi')
 
 for tweet in public_tweets:
 	print(tweet.text)
+	
+ def clean_tweet(self, tweet):
+        return '\n '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 	analysis = TextBlob(tweet.text)
 	print(analysis.sentiment)
